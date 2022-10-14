833bb3153b794ee4432635d5288902190b30d377
Nargel Velasco <nargel@sensortransport.com>
2022-10-13 21:32:08 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Test Build <code></code> and save to reload.
+         Test Build
         </p>
         <a
           className="App-link"
